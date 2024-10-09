9f6efb00fe3ec0cb26c3e539fa96575454a2a6d3
Faisal <faisalsagheerqureshi@gmail.com>
2024-10-09 11:17:57 +0500

    error

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ const apiKey = 'e68750d3d1690cae3a5e77daf9e25f72'
 async function city() {
 
     try {
-        
+
         const pokemonName = document.getElementById('cityName').value.toLowerCase();
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${pokemonName}&appid=${apiKey}`
-        const response = await fetch(apiUrl)
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${pokemonName}&appid=${apiKey}`;
+        const response = await fetch(apiUrl);
         // console.log(response)
         if (!response.ok) {
-            throw new Error("Not Found")
+            throw new Error("Not Found");
         }
-        const data = await response.json()
-        const cardTitle = document.getElementById('title')
-        const cardTemp = document.getElementById('temperature')
-        const tempData = data.main.temp
-        cardTemp.textContent =  `${(tempData - 273.15).toFixed(1)}C`
-        cardTitle.textContent = data.weather[0].description 
+        const data = await response.json();
+        const cardTitle = document.getElementById('title');
+        const cardTemp = document.getElementById('temperature');
+        const tempData = data.main.temp;
+        cardTemp.textContent = `${(tempData - 273.15).toFixed(1)}C`;
+        cardTitle.textContent = data.weather[0].description;
         // console.log(data)
     }
     catch (error) {
-        console.log(error)
+        const cardTitle = document.getElementById('title')
+        const cardTemp = document.getElementById('temperature')
+        cardTitle.textContent = "Error"
+        cardTemp.textContent = error.message
     }
 }
